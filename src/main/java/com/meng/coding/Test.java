492c31be69a080a612c03c24d245768fbe70aa89
mengmeng.sun <mengmeng.sun@mtime.com>
2020-08-12 11:12:24 +0800

    synchronzied

--- src/main/java/com/meng/coding/Test.java
+++ src/main/java/com/meng/coding/Test.java
@@ -10,16 +10,22 @@ public class Test {
 
 
     public static void main(String[] args) {
-//        System.out.println(1005569966%32 + 1);
-//        System.out.println(Math.abs("18611626991".hashCode())%32 + 1);
-        System.out.println(Math.abs("C0000000000000016763".hashCode())%8 + 1);
+        System.out.println(1110400105%32 + 1);
+        System.out.println(Math.abs("15201864878".hashCode())%32 + 1);
+        System.out.println(Math.abs("18310617894".hashCode())%32 + 1);
+        System.out.println(Math.abs("14299999902".hashCode())%32 + 1);
+        System.out.println(Math.abs("E304000000040611064".hashCode())%32 + 1);
 //        System.out.println(Math.abs("C0000000000004079596".hashCode())%32 + 1);
 //        System.out.println(Math.abs("C3810000000000004767".hashCode())%32 + 1);
 //        System.out.println(Math.abs("C3810000000000004773".hashCode())%32 + 1);
 //        System.out.println(Math.abs("E031000000152300444".hashCode())%32 + 1);
-        System.out.println(Math.abs("C3050000000008768801".hashCode())%32 + 1);
 //        System.out.println(Math.abs("18642659263".hashCode())%32 + 1);
 //        System.out.println(9190677%32 + 1);
     }
 
+
+//    E510000000140202555	1	510	2020-06-03 15:06:08	0	2
+//    E510000000140202577	1	510	2020-04-17 16:04:32	0	2
+//    E510000000140202775	1	510	2020-07-16 15:06:49	0	2
+//    E510000000140202797
 }
