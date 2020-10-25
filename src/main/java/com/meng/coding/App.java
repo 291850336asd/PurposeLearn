4b27c961b5a735d077bae844a78edc11585718c0
291850336asd <291850336@qq.com>
2020-10-25 14:31:14 +0800

    js 类型转换

--- src/main/java/com/meng/coding/App.java
+++ src/main/java/com/meng/coding/App.java
@@ -13,7 +13,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(Math.abs("E510000000140202669".hashCode())%8 +1);
+        System.out.println(Math.abs("15260802260".hashCode())%32 +1);
+        System.out.println(Math.abs("C3110000000042757813".hashCode())%32 +1);
+        System.out.println(1007341983%2 +1);
 
 
 
