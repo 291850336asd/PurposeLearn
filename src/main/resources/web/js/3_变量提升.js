8b6106f0f895c600ca4df26d6fc36a1d05e08f90
mengmeng.sun <mengmeng.sun@mtime.com>
2020-10-30 13:40:49 +0800

    js 变量提升 上下文

--- src/main/resources/web/js/3_变量提升.js
+++ src/main/resources/web/js/3_变量提升.js
@@ -225,3 +225,13 @@ function func(x,y=function anonymous1() {x =2;}) {
 func(5); //3
 console.log(x);//1
 
+
+var x = 1;
+function func(x,y=function anonymous1() {x =2;}) {
+    var x = 3;
+    var y = function anonymous2() {x =4;}
+    y();
+    console.log(x);
+}
+func(5); //4
+console.log(x);//1
