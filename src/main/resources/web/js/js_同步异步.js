acb81ac58218959da5b579e77d4b6909321da418
mengmeng.sun <mengmeng.sun@mtime.com>
2020-11-16 15:27:13 +0800

    js promise async await

--- src/main/resources/web/js/js_同步异步.js
+++ src/main/resources/web/js/js_同步异步.js
@@ -102,17 +102,17 @@ let p1 = new Promise((resolve, reject)=>{
    console.log("ok");
 });
 console.log("no");
-//结果 ok no
+//结果 ok no    得出结论： promise本身是同步的，是用来管理异步的
 
 
 // promise本身是同步的，是用来管理异步的，new promise会立即执行executor(resolve, reject){}函数，并创建返回promise实例
 // executor一般管理异步操作，操作成功执行resolve，让promise实例状态时成功并获取结果，失败执行执行reject让实例状态为失败并获取原因
-//   + resolve函数：函数执行会把promise状态改成成功
-//   + reject函数：函数执行会把promise状态改成失败
+//   + resolve函数：函数执行会把promise状态改成成功  异步执行
+//   + reject函数：函数执行会把promise状态改成失败   异步执行
 // promise实例属性
 //   +[[PromiseState]] promise状态  待定（pending）: 初始状态，既没有被兑现，也没有被拒绝。
 //          + 待定（pending）: 初始状态，既没有被兑现，也没有被拒绝。
-//          + 已兑现（fulfilled）: 意味着操作成功完成。
+//          + 已兑现（fulfilled/ resolved）: 意味着操作成功完成。
 //          + 已拒绝（rejected）: 意味着操作失败。
 //   +[[PromiseResult]] promise结果
 //          + 初始值undefined
@@ -126,7 +126,157 @@ let p1 = new Promise((resolve, reject)=>{
         ran < 0.5 ? reject("NO") : resolve("ok");
     },1000);
 });
+////
+let p1 = new Promise((resolve, reject)=>{
+    resolve(100);// 改变状态后，下面改变状态的代码就无用了
+    reject(0);
+});
+//
+/**
+ * Promise.prototype
+ *   + then[A,B]: 基于then方法存放两个回调函数，当promise状态执行成功时执行A,失败执行B，返回新promise实例
+ *       新实例的状态基于then[A,B]执行的结果决定的，AB不报错，则状态是成功，否则是失败，结果是AB函数的返回值
+ *       特殊情况，若AB返回的是一个promise实例，则返回的promise直接决定p2的状态和结果
+ *   + catch  相当于 then(null, reason=>{})
+ *   + finally:  Appends a handler to the promise, and returns a new promise that is resolved when the original promise is resolved. The handler is called when the promise is settled, whether fulfilled or rejected.
+ *
+ *
+ */
+////then
+let p2 = p1.then(result =>{
+    console.log(`成功了 -> ${result}`);
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+});
+console.log("haha");//先输出haha,再输出resolve函数/reject函数结果
+//
+let p2 = p1.then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return "result -> " + result; //返回值 赋值给PromiseResult
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return "reason -> " + reason;  //返回值 赋值给PromiseResult
+});
+////
+let p1 = new Promise((resolve, reject)=>{
+    resolve(100);// 改变状态后，下面改变状态的代码就无用了
+    reject(20);
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return result*10;
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10;
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return Promise.reject(result * 10);
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10;
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return result*10;
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10;
+});
+// 成功了 -> 100
+// 成功了 -> 1000
+// 失败了 -> 10000
+////
+let p1 = new Promise((resolve, reject)=>{
+    reject(20);   //失败返回20
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return result*10;
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10; // 此时返回new Promise ,B函数方法未报错，则返回新promise状态为成功
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return Promise.reject(result * 10);
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10;
+}).then(result =>{
+    console.log(`成功了 -> ${result}`);
+    return result*10;
+}, reason => {
+    console.log(`失败了 -> ${reason}`);
+    return reason/10;  // 此时返回new Promise ,B函数方法未报错，则返回新promise状态为成功
+});
+// 失败了 -> 20
+// 成功了 -> 2
+// 失败了 -> 20
 
+////catch 相当于 then(null, reason=>{})
+//正常then(A,B)，如果其中一个没有传，则会‘顺延’
+//  +[A]没有传递，则会查找下一个then中的A函数   A->null,类似于 result=>{return result}
+//  +[B]没有传递，则会查找下一个then中的B函数   B->null,类似于 reason=>{return Promise.reject(reson)}
+let p1 = new Promise((resolve, reject)=>{
+    resolve(100);   //失败返回20
+}).then(null, reason => {
+    console.log(`失败了1 -> ${reason}`);
+    return reason/10; // 此时返回new Promise ,B函数方法未报错，则返回新promise状态为成功
+}).then(result =>{
+    console.log(`成功了2 -> ${result}`);
+    return Promise.reject(result * 10);
+}, reason => {
+    console.log(`失败了2 -> ${reason}`);
+    return reason/10;
+});
+// 输出成功了2 -> 100，然后报错Uncaught (in promise) 1000,抛出的异常不会影响其他代码执行,并且异常通过trycatch吃掉和await不同
+//
+let p1 = new Promise((resolve, reject)=>{
+    resolve(100);   //失败返回20
+}).then(null, reason => {
+    console.log(`失败了1 -> ${reason}`);
+    return reason/10; // 此时返回new Promise ,B函数方法未报错，则返回新promise状态为成功
+}).then(result =>{
+    console.log(`成功了2 -> ${result}`);
+    return Promise.reject(result * 10);
+}, reason => {
+    console.log(`失败了2 -> ${reason}`);
+    return reason/10;
+}).catch(reason => {
+    console.log(reason);
+});
+// 成功了2 -> 100
+// 1000
+
+////finally
+Promise.resolve(10).finally(()=>{
+    console.log("finally");
+});
+console.log("ss");
+// ss finally
+//
+let p1 = new Promise((resolve, reject)=>{
+    console.log("ok");
+}).finally(()=>{
+    console.log("finally"); // finally不会输出。因为未调用resolve或reject
+});
+console.log("no");
+
+
+/**
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise 静态方法
+ * Promise作为对象
+ *   + resolve(value) 返回一个状态为成功的promise实例
+ *   + reject(reason) 返回一个状态为失败的promise实例
+ *   + all(iterable)
+ *      这个方法返回一个新的promise对象，该promise对象在iterable参数对象里所有的promise对象都成功的时候才会触发成功，
+ *      一旦有任何一个iterable里面的promise对象失败则立即触发该promise对象的失败。
+ *      这个新的promise对象在触发成功状态以后，会把一个包含iterable里所有promise返回值的数组作为成功回调的返回值，
+ *      顺序跟iterable的顺序保持一致；
+ *      如果这个新的promise对象触发了失败状态，它会把iterable里第一个触发失败的promise对象的错误信息作为它的失败错误信息。
+ *      Promise.all方法常被用于处理多个promise对象的状态集合
+ *   + race(iterable)
+ *      当iterable参数里的任意一个子promise被成功或失败后，父promise马上也会用子promise的成功返回值或失败详情作为结果
+ *   ...
+ *
+ */
+Promise.reject(100);
 
 //回调地狱
 let data={};
@@ -198,15 +348,6 @@ queryInfo().then(result =>{
 })();
 
 
-
-
-
-
-
-
-
-
-
 //
 //...........async await........es7语法糖
 //async控制当前函数返回的是promise 和then很相似，函数执行不报错返回成功的promise实例，报错则返回失败的
@@ -229,11 +370,96 @@ function fn() {
     })
 };
 async function func() {
+    console.log("func")
     let res = await fn(); //等待promise执行完成，在执行下面的代码
     console.log(res);// ok
 }
+func();
+console.log("NO");
+//func NO 异步完成 ok
+////
+async function func() {
+    console.log(100);
+    return 10;
+}
+async function fn() {
+    let res = await func(); //等待promise执行完成且状态为成功时，在执行下面的代码且下面代码被当做微任务处理
+    console.log(res);// ok  微任务
+}
+fn();
+console.log("NO");
+// 100 NO 10
+
+////
+(async function() {
+    let x = await  10;//非promise就认为是成功的
+    console.log(x);
+    let y = await Promise.resolve(20);
+    console.log(y);
+    try{
+        let z = await Promise.reject(30); // await返回的promise失败，则当前函数中后面的代码不会执行
+        console.log(z);
+    }catch (e) {
+        console.log(e);// 30
+    }
+})();
 
 
+////
+async function async1() {
+    console.log('async1 start');
+    await async2(); //await下面的代码整体放到微任务中
+    console.log('async1 end');// 微任务代码
+}
+async function async2() {
+    console.log('async2');
+}
+console.log('script start');
+setTimeout(function() {
+    console.log('setTimeout');
+}, 0)
+async1();
+new Promise(function(resolve) {
+    console.log('promise1');
+    resolve();
+}).then(function() {
+    console.log('promise2');
+});
+console.log('script end');
+//输出结果：
+//script start    async1 start   async2   promise1  script end  async1 end  promise2  setTimeout
 
-
-
+////
+function func1(){
+    console.log('func1 start');
+    return new Promise(resolve=>{
+        resolve('OK');
+    });
+}
+function func2(){
+    console.log('func2 start');
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve('OK');
+        },10);
+    });
+}
+console.log(1);
+setTimeout(async () => {
+    console.log(2);
+    await func1();
+    console.log(3);
+}, 20);
+for (let i = 0; i < 90000000; i++) {} //循环大约要进行80MS左右
+console.log(4);
+func1().then(result=>{
+    console.log(5);
+});
+func2().then(result=>{
+    console.log(6);
+});
+setTimeout(() => {
+    console.log(7);
+}, 0);
+console.log(8);
+//输出结果：1  4  func1 start func2 start  8 5 2 func1 start 3 7 6
