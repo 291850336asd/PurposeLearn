29bc6a797c56ad26105ee57e0d8e48f72d9dce98
291850336asd <291850336@qq.com>
2020-11-20 07:05:37 +0800

    拖拽

--- src/main/resources/web/js/13_4_拖拽.js
+++ src/main/resources/web/js/13_4_拖拽.js
@@ -1,3 +1,13 @@
 $(function () {
+    let box = $('.box');
+    var boundingClientRect = box.getBoundingClientRect(); //距离当前视口的距离
 
+    const down = function down(ev) {
+    };
+    const up = function up(ev) {
+
+    };
+
+    box.addEventListener('mousedown', down);
+    box.addEventListener('mousedup', down);
 });
