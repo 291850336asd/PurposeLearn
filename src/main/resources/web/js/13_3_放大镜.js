ba4d508a7e7d8ed0c73fb98fdc075d4aaa5fae82
mengmeng.sun <mengmeng.sun@mtime.com>
2020-11-19 18:19:33 +0800

    js 事件机制

---
+++ src/main/resources/web/js/13_3_放大镜.js
@@ -0,0 +1,11 @@
+$(function () {
+    let $magnifer = $(".magnifer");
+    let $abbre = $magnifer.find(".abbre");
+    let $mask = $abbre.find(".mask");
+    let $detail = $magnifer.find(".detail");
+    let $detailImg = $detail.find("img");
+
+    $abbre.on("mouseenter", function (ev) {
+        
+    })
+});
