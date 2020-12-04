3148a54c3e20c1f616ce397a7d392d74a4d6e424
291850336asd <291850336@qq.com>
2020-12-04 19:33:46 +0800

    webpack js css 压缩

--- src/main/resources/web.webpack/conf/webpack.base.js
+++ src/main/resources/web.webpack/conf/webpack.base.js
@@ -11,26 +11,23 @@ module.exports = {
     mode: 'development', // 控制环境 development 默认 production
     entry: {
         index: './src/index.js',
-        indexSecond:'./src/indexSecond.js',
-        common:'./src/common.js'
+        indexSecond: './src/indexSecond.js',
+        common: './src/common.js'
     },   // 默认值为 ./src/index.js    来指定一个入口起点（或多个入口起点）。
-    output:{
+    output: {
         // output 属性告诉 webpack 在哪里输出它所创建的 bundles，以及如何命名这些文件，默认值为 ./dist,  基本上，整个应用程序结构，都会被编译到你指定的输出路径的文件夹中
         path: path.resolve(__dirname, '../dist'),  //需要配置绝对路径
         filename: '[name].[hash:5].js'
     },
-    plugins:[
+    plugins: [
         new CleanWebpackPlugin(),
         // new OptimizeCssAssetsPlugin(),
-        new TerserPlugin({
-            parallel: true,
-        }),
         new MiniCssExtractPlugin({
-            filename:'css/[name][hash:5].css'
+            filename: 'css/[name][hash:5].css'
         })
     ],
-    module:{
-        rules:[
+    module: {
+        rules: [
             // {
             //     test: /\.css$/,
             //     //css-loader把css转成js语法，style-loader把css转成的js语法，插入到页面中
@@ -64,7 +61,7 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader','less-loader','postcss-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader', 'postcss-loader']
             },
             {
                 test: /\.(png|jpg|jpeg|gif|jfif|icon)$/i,
@@ -72,7 +69,7 @@ module.exports = {
                     {
                         loader: 'url-loader',
                         options: {
-                            name:'img/[name].[ext]', //不设置自动在dist生成名字
+                            name: 'img/[name].[ext]', //不设置自动在dist生成名字
                             limit: 8192
                             //url-loader 功能类似于 file-loader，但是在文件大小（单位 byte）低于指定的限制时，可以返回一个 DataURL。
                             //当图片大小小于limit时，则直接base64  减少服务器请求
@@ -87,14 +84,13 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin({
+            new CssMinimizerPlugin(), //css压缩
+            new TerserPlugin({    // js压缩
                 parallel: true,
             })
-        ],
-
+        ]
     }
-
+};
 //单入口
 // module.exports = {
 //     // 这个对象里面都是webpack的配置项
