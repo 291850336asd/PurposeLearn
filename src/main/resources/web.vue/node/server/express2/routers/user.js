1561165b88ed49dbf2b4df456184bd17c0ce83cb
291850336asd <291850336@qq.com>
2020-12-28 11:18:15 +0800

    node express

--- src/main/resources/web.vue/node/server/express2/routers/user.js
+++ src/main/resources/web.vue/node/server/express2/routers/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
 let {listInfos} = require('../../data/list');
 const route = express.Router();
-
-
-
+const user = {id:1, name:'usertest',phone:'134xxxxxxxx'}
+//登录
+route.post('/login',(req, res)=> {
+    let { account, password, yupe} = req.body;
+    res.send(user);
+});
+//校验是否登录
+route.get('/login',(req, res)=> {
+    let { account, password, yupe} = req.body;
+    res.send({id:1, name:'usertest',phone:'134xxxxxxxx'});
+});
 route.get("/list",(req, res) => {
     let { page = 1, size=5 } = req.query;
     res.send(listInfos.slice((page-1)*size, page*size));
