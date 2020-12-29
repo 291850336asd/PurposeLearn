577a53303ac89d41114ec3c99cc931f7065e80b7
mengmeng.sun <mengmeng.sun@mtime.com>
2020-12-29 17:50:11 +0800

    vue eer

--- src/main/resources/web.vue/ssr/ssr/server/express-ssr.js
+++ src/main/resources/web.vue/ssr/ssr/server/express-ssr.js
@@ -1,19 +1,26 @@
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
 const server = express();
 const Vue = require('vue');
 
 //获取渲染实例
 const { createRenderer } = require("vue-server-renderer");
-const render = createRenderer();
+const render = createRenderer({});
 
 
 //处理favicon
 const favicon = require('serve-favicon');
 server.use(favicon('../public/favicon.ico'))
-server.get('/', (req, res) => {
+server.get('*', (req, res) => {
+    const template = req.url.substr(1) || 'index';
+    // console.log( req.url,template)
+    // 加载模板
+    const buffer = fs.readFileSync(path.resolve(__dirname, '../public/' + template + '.html'));
+
    const app = new Vue({
-       template:"<div @click='onClick'> {{msg}} </div>",  //无法交互
+       // template:"<div @click='onClick'> {{msg}} </div>",  //无法交互
+       template: buffer.toString(),  //无法交互
        data(){
            return {
              msg: "vue ssr"
