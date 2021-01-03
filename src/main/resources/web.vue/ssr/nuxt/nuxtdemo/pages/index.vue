93ec58fb595f932b15cfcd33e2decb8535673749
291850336asd <291850336@qq.com>
2021-01-04 06:57:59 +0800

    ssr nuxt

--- src/main/resources/web.vue/ssr/nuxt/nuxtdemo/pages/index.vue
+++ src/main/resources/web.vue/ssr/nuxt/nuxtdemo/pages/index.vue
@@ -1,23 +1,24 @@
 <template>
   <section class="container">
     <div>
+      <h3>首页</h3>
       <app-logo/>
-      <h1 class="title">
-        nuxtdemo   {{ a }} -  {{ b }}
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <!--<h1 class="title">-->
+        <!--nuxtdemo   {{ a }} -  {{ b }}-->
+      <!--</h1>-->
+      <!--<h2 class="subtitle">-->
+        <!--Nuxt.js project-->
+      <!--</h2>-->
+      <!--<div class="links">-->
+        <!--<a-->
+          <!--href="https://nuxtjs.org/"-->
+          <!--target="_blank"-->
+          <!--class="button&#45;&#45;green">Documentation</a>-->
+        <!--<a-->
+          <!--href="https://github.com/nuxt/nuxt.js"-->
+          <!--target="_blank"-->
+          <!--class="button&#45;&#45;grey">GitHub</a>-->
+      <!--</div>-->
     </div>
   </section>
 </template>
