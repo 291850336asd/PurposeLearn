7e7ff719ba5d5149a790b8a92d42dbd06768710d
mengmeng.sun <mengmeng.sun@mtime.com>
2021-01-05 14:20:55 +0800

    nuxt loading

--- src/main/resources/web.vue/ssr/nuxt/nuxtdemo/layouts/app-header.vue
+++ src/main/resources/web.vue/ssr/nuxt/nuxtdemo/layouts/app-header.vue
@@ -1,5 +1,8 @@
 <template>
     <nav>
+      <el-menu default-active="activeIndex" @select="handlerSelect" mode="horizontal">
+        <el-menu-item v-for="(item,index) of navs" :key="index" >{{item.title}}</el-menu-item>
+      </el-menu>
       <nuxt-link to="/index" active-class="app_header--active">首页</nuxt-link>
       <nuxt-link to="/goods" active-class="app_header--active">商品</nuxt-link>
       <nuxt-link to="/goods2" active-class="app_header--active">商品2同级展示</nuxt-link>
@@ -9,6 +12,46 @@
     </nav>
 </template>
 
+<script>
+  export default {
+    data(){
+      return {
+        activeIndex: '-1',
+        navs:[
+          {path:'/index', title:'首页'},
+          {path:'/user', title:'用户'},
+          {path:'/goods', title:'商品'}
+        ]
+      }
+    },
+    watch:{
+      $route:{
+        immediate: true,
+        handler(route){
+          if(route.fullPath == '/'){
+            this.$router.push({name:'root'});
+            return
+          }
+          var isfind = false;
+          this.navs.map((item, index)=>{
+            if(route.fullPath == item.path){
+              this.activeIndex = index + '';
+              isfind = true;
+            }
+          });
+          if(!isfind){
+            this.activeIndex = '-1';
+          }
+        }
+      }
+    },
+    methods:{
+      handlerSelect(key){
+        this.$router.push(this.navs[parseInt(key)].path);
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .app_header--active{
