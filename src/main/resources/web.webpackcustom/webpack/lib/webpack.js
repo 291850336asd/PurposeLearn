c4864032f53f3a7ed1ecafa6911b05ab546ed589
291850336asd <291850336@qq.com>
2021-01-25 07:27:14 +0800

    webpack custom

--- src/main/resources/web.webpackcustom/webpack/lib/webpack.js
+++ src/main/resources/web.webpackcustom/webpack/lib/webpack.js
@@ -2,32 +2,60 @@ const fs = require("fs");
 const path = require("path");
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
+const { transformFromAst } = require("@babel/core");
+
 module.exports = class webpack {
     constructor(options){
         // console.log(options);
         const { entry,output } = options;
         this.entry = entry;
         this.output = output;
+        this.modules = [];
     }
 
     run(){
-        this.parse(this.entry);
+        //开始分析入口模块的内容
+        const info = this.parse(this.entry);
+        // console.log(info);
+        //递归分析其他模块
+        this.modules.push(info);
+        this.modules.forEach(item=>{
+            const { dependencies } = item;
+            if(dependencies){
+                for (let j in dependencies){
+                    const deInfo = this.parse(dependencies[j]);
+                     // console.log(deInfo);
+                    this.modules.push(deInfo);
+
+                }
+            }
+
+        })
+        console.log( this.modules)
     }
 
     parse(entryFile){
         // 开始分析入口文件的内容
         const content = fs.readFileSync(entryFile, "utf-8");
-        console.log(content);
+        // console.log(content);
         // 抽象语法树
         var ast = parser.parse(content, {
             sourceType:"module"
         });
         // console.log(ast.program.bod
         // y);
+        const dependencies = {};
         traverse(ast, {
             ImportDeclaration({node}) {
-                console.log(path.join(path.dirname(entryFile),node.source.value));
+                const newPathName = "./" + path.join(path.dirname(entryFile),node.source.value);
+                dependencies[node.source.value] = newPathName;
             }
         });
+        const  { code } = transformFromAst(ast,null, {
+            presets:["@babel/preset-env"]
+        })
+       return {
+            entryFile,dependencies, code
+       };
     }
 }
